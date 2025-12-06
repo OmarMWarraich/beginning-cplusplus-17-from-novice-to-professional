407058a0ddf381d95ba0617ed35f64f9a7a90c2e
OmarMWarraich <earthling986@gmail.com>
2025-12-06 19:39:04 +0500

    feat: added more assertions

--- tests/betterOutputMultipleTests/main.cpp
+++ tests/betterOutputMultipleTests/main.cpp
@@ -1,28 +1,32 @@
 #include <cppunit/TestCase.h>
 #include <cppunit/TestCaller.h>
 #include <cppunit/ui/text/TestRunner.h>
+#include <vector>
 
 class FixtureTests : public CppUnit::TestFixture {
 public:
 
-  void setUp() override
-  {
-    printf("Passed\n");
-  }
-  void tearDown() override
-  {
-    printf("ailed\n");
-  }
+  // void setUp() override
+  // {
+  //   printf("Passed\n");
+  // }
+  // void tearDown() override
+  // {
+  //   printf("ailed\n");
+  // }
 
   void testAddition()
   {
     CPPUNIT_ASSERT(2 + 4 == 6);
-    // CPPUNIT_ASSERT(2 + 1 == 4);
+    // CPPUNIT_ASSERT_MESSAGE("Failed", 3 + 4 == 8);
   }
   void testLogic()
   {
-    CPPUNIT_ASSERT(false == false);
+    // CPPUNIT_ASSERT(false == false);
     // CPPUNIT_ASSERT(true == false);
+    std::vector<int> v;
+    // v.push_back(10);
+    CPPUNIT_ASSERT_NO_THROW_MESSAGE("terribly failed!", v.at(0));
   }
 };
 
